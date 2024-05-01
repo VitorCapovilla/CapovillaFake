5bd590d08fdc80beb720d5c230cb06b75ed09d90
VitorCapovillaMast <vitor.capovilla@mastsolucoes.com>
2024-04-30 23:57:45 -0300

    First Commit

---
+++ src/main/java/br/com/vitorcapovilla/capovillafake/CapovillaFake.java
@@ -0,0 +1,17 @@
+package br.com.vitorcapovilla.capovillafake;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CapovillaFake extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
